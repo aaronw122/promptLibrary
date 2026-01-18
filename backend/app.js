1705e8676512c03feac649f189fca453f59b1151
aaron <ajwills@umich.edu>
2026-01-18 16:16:40 -0600

    BE working

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,26 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const promptsRouter = require('./controllers/prompts')
 
-const app = express
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, {family: 4})
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/prompts', promptsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
